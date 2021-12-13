f4c9c4b45bdf195237383e113dfb2b539a681ac5
Raimondas Galvelis <r.galvelis@acellera.com>
2021-12-13 17:03:38 +0100

    Test if a PyTorch module receives corrects arguments

--- python/tests/TestTorchForce.py
+++ python/tests/TestTorchForce.py
@@ -3,6 +3,9 @@ import simtk.unit as unit
 import openmmtorch as ot
 import numpy as np
 import unittest
+import pytest
+import torch as pt
+from tempfile import NamedTemporaryFile
 
 class TestTorchForce(unittest.TestCase):
 
@@ -29,6 +32,54 @@ class TestTorchForce(unittest.TestCase):
         expectedEnergy = np.sum(positions*positions)
         assert np.allclose(expectedEnergy, state.getPotentialEnergy().value_in_unit(unit.kilojoules_per_mole))
         assert np.allclose(-2*positions, state.getForces(asNumpy=True))
+
+
+@pytest.mark.parametrize('deviceString', ['cpu', 'cuda:0', 'cuda:1'])
+@pytest.mark.parametrize('precision', ['single', 'mixed', 'double'])
+def testModuleArguments(deviceString, precision):
+
+    class TestModule(pt.nn.Module):
+
+        def __init__(self, device, dtype, positions):
+            super().__init__()
+            self.device = device
+            self.dtype = dtype
+            self.positions = pt.tensor(positions).to(self.device).to(self.dtype)
+
+        def forward(self, positions):
+            assert positions.device == self.device
+            assert positions.dtype == self.dtype
+            assert pt.all(positions == self.positions)
+            return pt.sum(positions)
+
+    with NamedTemporaryFile() as fd:
+
+        numParticles = 10
+        system = mm.System()
+        positions = np.random.rand(numParticles, 3)
+        for _ in range(numParticles):
+            system.addParticle(1.0)
+
+        device = pt.device(deviceString)
+        if device.type == 'cpu' or precision == 'double':
+            dtype = pt.float64
+        else:
+            dtype = pt.float32
+        module = TestModule(device, dtype, positions)
+        pt.jit.script(module).save(fd.name)
+        force = ot.TorchForce(fd.name)
+        system.addForce(force)
+
+        integrator = mm.VerletIntegrator(1.0)
+        platform = mm.Platform.getPlatformByName(device.type.upper())
+        properties = {}
+        if device.type == 'cuda':
+            properties['DeviceIndex'] = str(device.index)
+            properties['Precision'] = precision
+        context = mm.Context(system, integrator, platform, properties)
+
+        context.setPositions(positions)
+        context.getState(getEnergy=True, getForces=True)
 
 
 if __name__ == '__main__':
