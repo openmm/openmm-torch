07b39b917e7c0f4015efef539e230e2804e12d20
Raimondas Galvelis <r.galvelis@acellera.com>
2022-05-26 11:53:41 +0200

    Simplify the test models

--- tests/generate.py
+++ tests/generate.py
@@ -2,21 +2,21 @@ import torch as pt
 
 class Central(pt.nn.Module):
     def forward(self, pos):
-        return pos.pow(2).sum(dim=1).abs().sum()
+        return pos.pow(2).sum()
 
 class Forces(pt.nn.Module):
     def forward(self, pos):
-        return pos.pow(2).sum(dim=1).abs().sum(), -2*pos
+        return pos.pow(2).sum(), -2 * pos
 
 class Global(pt.nn.Module):
     def forward(self, pos, k):
-        return k * pos.pow(2).sum(dim=1).abs().sum()
+        return k * pos.pow(2).sum()
 
 class Periodic(pt.nn.Module):
     def forward(self, pos, box):
         box = box.diagonal().unsqueeze(0)
         pos = pos - (pos / box).floor() * box
-        return pos.pow(2).sum(dim=1).abs().sum()
+        return pos.pow(2).sum()
 
 pt.jit.script(Central()).save('central.pt')
 pt.jit.script(Forces()).save('forces.pt')
