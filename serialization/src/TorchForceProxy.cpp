73b52220f8d18e26612462ae24fe3227673ae471
RaulPPealez <raulppelaez@gmail.com>
2023-01-20 16:06:49 +0100

    Encode the model file contents when serializing TorchForce

--- serialization/src/TorchForceProxy.cpp
+++ serialization/src/TorchForceProxy.cpp
@@ -32,11 +32,60 @@
 #include "TorchForceProxy.h"
 #include "TorchForce.h"
 #include "openmm/serialization/SerializationNode.h"
-#include <string>
+#include<string>
+#include<fstream>
+#include<sstream>
+#include<iostream>
 
 using namespace TorchPlugin;
 using namespace OpenMM;
 using namespace std;
+
+namespace detail{
+  // Based in the answers in https://stackoverflow.com/a/34571089/5155484
+  static constexpr auto base64_alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+  static std::string base64_encode(const std::string &in){
+    std::string out;
+    unsigned val=0;
+    int valb=-6;
+    for (auto& c : in) {
+      val = (val<<8) + c;
+      valb += 8;
+      while (valb>=0) {
+	out.push_back(base64_alphabet[(val>>valb)&0x3F]);
+	valb-=6;
+      }
+    }
+    if (valb>-6) out.push_back(base64_alphabet[((val<<8)>>(valb+8))&0x3F]);
+    while (out.size()%4) out.push_back('=');
+    return out;
+  }
+
+  static std::string base64_decode(const std::string &in) {
+    std::string out;
+    std::vector<int> T(256,-1);
+    for (int i=0; i<64; i++) T[base64_alphabet[i]] = i;
+    unsigned val=0;
+    int valb=-8;
+    for (auto& c : in) {
+      if (T[c] == -1) break;
+      val = (val<<6) + T[c];
+      valb += 6;
+      if (valb>=0) {
+	out.push_back(char((val>>valb)&0xFF));
+	valb-=8;
+      }
+    }
+    return out;
+  }
+
+  std::string encodeFromFileName(const std::string &fileName){
+    std::stringstream ss;
+    ss << std::ifstream(fileName).rdbuf();
+    const auto fileContents = ss.str();
+    return base64_encode(fileContents);
+  }
+}
 
 TorchForceProxy::TorchForceProxy() : SerializationProxy("TorchForce") {
 }
@@ -45,6 +94,12 @@ void TorchForceProxy::serialize(const void* object, SerializationNode& node) con
     node.setIntProperty("version", this->version);
     const TorchForce& force = *reinterpret_cast<const TorchForce*>(object);
     node.setStringProperty("file", force.getFile());
+    try{
+      node.setStringProperty("encodedFileContents", detail::encodeFromFileName(force.getFile()));
+    }
+    catch(...){
+      throw OpenMMException("Could not serialize model file.");
+    }
     node.setIntProperty("forceGroup", force.getForceGroup());
     node.setBoolProperty("usesPeriodic", force.usesPeriodicBoundaryConditions());
     node.setBoolProperty("outputsForces", force.getOutputsForces());
@@ -55,9 +110,28 @@ void TorchForceProxy::serialize(const void* object, SerializationNode& node) con
 }
 
 void* TorchForceProxy::deserialize(const SerializationNode& node) const {
-    TorchForce* force = new TorchForce(node.getStringProperty("file"));
-    if (node.getIntProperty("version") != this->version)
-      throw OpenMMException("Unsupported version number");
+    int storedVersion = node.getIntProperty("version");
+    if (storedVersion > this->version)
+        throw OpenMMException("Unsupported version number");
+    std::string fileName;
+    const std::string storedEncodedFile = node.getStringProperty("encodedFileContents", "");
+    if(storedVersion == 1){
+      fileName = node.getStringProperty("file");
+      if(not storedEncodedFile.empty()){
+	const auto encodedFileContents = detail::encodeFromFileName(fileName);
+	if(storedEncodedFile.compare(encodedFileContents) != 0){
+	  throw OpenMMException("The provided model file does not match the stored one");
+	}
+      }
+    }
+    if(storedVersion > 2){
+      if(not node.getStringProperty("file", "").empty()){
+	throw OpenMMException("Serializer version is incompatible with file parameter");
+      }
+      fileName = std::tmpnam(nullptr); //A unique filename
+      std::ofstream(fileName)<<detail::base64_decode(storedEncodedFile);
+    }
+    TorchForce* force = new TorchForce(fileName);
     if (node.hasProperty("forceGroup"))
         force->setForceGroup(node.getIntProperty("forceGroup", 0));
     if (node.hasProperty("usesPeriodic"))
