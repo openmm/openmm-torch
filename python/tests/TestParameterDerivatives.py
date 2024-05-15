ded655914fce7ca630a55756dca18eba7b5e7011
RaulPPealez <raulppelaez@gmail.com>
2024-05-15 10:15:17 +0200

    typo

--- python/tests/TestParameterDerivatives.py
+++ python/tests/TestParameterDerivatives.py
@@ -40,7 +40,7 @@ def testParameterEnergyDerivatives(use_cv_force, platform):
     parameter = 1.0
     force.addGlobalParameter("parameter", parameter)
     # Enable energy derivatives for the parameter
-    force.addEnergyParameterDerivatives("parameter")
+    force.addEnergyParameterDerivative("parameter")
     force.setOutputsForces(True)
     if use_cv_force:
         # Wrap TorchForce into CustomCVForce
